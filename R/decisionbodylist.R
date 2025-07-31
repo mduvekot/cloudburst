7c5af8ce0a8be00cda431d64dfe470793c317ae8
Michiel Duvekot <michiel@duvekot.ca>
2025-07-31 16:07:34 -0400

    add agendaitem, move functions into R/ add roxygen documentation

--- R/decisionbodylist.R
+++ R/decisionbodylist.R
@@ -1,5 +1,9 @@
 library(jsonlite)
-
+#' decision body list
+#' 
+#' @param termid the current term as an integer, (8 is 2022-2026)
+#' @return a data frame
+#' @export
 decisonbodylist <- function (term_id = 8) {
   json <- 
 fromJSON(paste0("https://secure.toronto.ca/council/api/multiple/decisionbody-list.json?termId=", term_id))
